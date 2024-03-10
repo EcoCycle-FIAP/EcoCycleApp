bc386220d654527db80042d0e486a9e3b12f5e03
adrian-siv <adrian.ssiv@gmail.com>
2024-03-10 15:44:59 -0300

    fix(MainActivity): delete unnecessary code

--- app/src/main/java/br/com/ecocycle/ecocycleapp/MainActivity.kt
+++ app/src/main/java/br/com/ecocycle/ecocycleapp/MainActivity.kt
@@ -3,28 +3,12 @@ package br.com.ecocycle.ecocycleapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.unit.dp
-import br.com.ecocycle.ecocycleapp.components.InputTextoPadrao
 import br.com.ecocycle.ecocycleapp.ui.theme.EcoCycleAppTheme
-import br.com.ecocycle.ecocycleapp.ui.theme.Jaldi
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
