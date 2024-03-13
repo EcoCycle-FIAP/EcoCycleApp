d98e0ba8ffac204f87cbb84899d2062494cba3be
adrian-siv <adrian.ssiv@gmail.com>
2024-03-13 19:34:57 -0300

    fix(ModalPadrao): finish component details

--- app/src/main/java/br/com/ecocycle/ecocycleapp/components/ModalPadrao.kt
+++ app/src/main/java/br/com/ecocycle/ecocycleapp/components/ModalPadrao.kt
@@ -1,37 +1,25 @@
 package br.com.ecocycle.ecocycleapp.components
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.AlertDialogDefaults
-import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.TextUnit
-import androidx.compose.ui.unit.dp
 import br.com.ecocycle.ecocycleapp.R
 
 @Composable
 fun ModalPadrao(
     title: String,
     titleFontSize: TextUnit,
-    textComponent: @Composable() (() -> Unit),
+    textComponent: @Composable (() -> Unit),
     onDismissRequest: () -> Unit,
-    confirmButton: @Composable() (() -> Unit)? = {},
+    confirmButton: @Composable (() -> Unit)? = {},
 ) {
     AlertDialog(
         title = {
@@ -50,12 +38,12 @@ fun ModalPadrao(
         dismissButton = {
             Column(
                 horizontalAlignment = Alignment.CenterHorizontally,
-                modifier = Modifier.fillMaxWidth()
+                modifier = Modifier.fillMaxWidth(),
             ) {
                 BotaoPadrao(
-                    onClick = { /*TODO*/ },
+                    onClick = onDismissRequest,
                     text = "OK",
-                    width = 100,
+                    width = 80,
                     contentFontSize = 12,
                     height = 35,
                 )
