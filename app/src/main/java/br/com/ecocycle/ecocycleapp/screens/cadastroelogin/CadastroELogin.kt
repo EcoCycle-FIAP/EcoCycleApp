b477bc1ac27fcd16b1939aad646ee73e0833ca80
NicoleTuccio <nicole.tuccio.ribeiro@gmail.com>
2024-03-16 11:12:23 -0300

    Commit TK21 update 1

--- app/src/main/java/br/com/ecocycle/ecocycleapp/screens/cadastroelogin/CadastroELogin.kt
+++ app/src/main/java/br/com/ecocycle/ecocycleapp/screens/cadastroelogin/CadastroELogin.kt
@@ -12,8 +12,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.OutlinedButton
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
