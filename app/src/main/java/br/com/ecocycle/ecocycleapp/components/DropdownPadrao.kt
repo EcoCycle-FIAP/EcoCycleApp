f735d7c3cef26f9f9376c711933ceb9526e38de2
adrian-siv <adrian.ssiv@gmail.com>
2024-03-17 00:10:01 -0300

    feat(DropdownPadrao): create component

--- app/src/main/java/br/com/ecocycle/ecocycleapp/components/DropdownPadrao.kt
+++ app/src/main/java/br/com/ecocycle/ecocycleapp/components/DropdownPadrao.kt
@@ -1,2 +1,90 @@
 package br.com.ecocycle.ecocycleapp.components
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import br.com.ecocycle.ecocycleapp.R
+
+@Composable
+fun DropdownPadrao(
+    label: String,
+    placeholder: String,
+    optionsList: List<String>
+) {
+    var expanded by remember { mutableStateOf(false) }
+    var selectedItem by remember { mutableStateOf("") }
+    var inputWidth by remember { mutableIntStateOf(0) }
+
+    val trailingIcon = if (expanded) {
+        R.drawable.dropdown_arrow_up
+    } else {
+        R.drawable.dropdown_arrow_down
+    }
+
+    val trailingIconDescription = if (expanded) {
+        "Seta para cima"
+    } else {
+        "Seta para baixo"
+    }
+
+    Column(
+        modifier = Modifier
+            .padding(16.dp)
+            .fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        InputTextoPadrao(
+            label = label,
+            placeholder = placeholder,
+            value = selectedItem,
+            onValueChange = { selectedItem = it },
+            modifier = Modifier.onGloballyPositioned { coordinates ->
+                inputWidth = coordinates.size.width
+            },
+            trailingIcon =
+            {
+                Icon(
+                    painter = painterResource(id = trailingIcon),
+                    contentDescription = trailingIconDescription,
+                    Modifier
+                        .clickable { expanded = !expanded }
+                        .size(35.dp)
+                )
+            },
+            readOnly = true
+        )
+        DropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false },
+            modifier = Modifier.width(inputWidth.dp)
+        ) {
+            optionsList.forEach { label ->
+                DropdownMenuItem(
+                    text = { Text(text = label) },
+                    onClick = {
+                        selectedItem = label
+                        expanded = false
+                    }
+                )
+            }
+        }
+    }
+}
--- app/src/main/java/br/com/ecocycle/ecocycleapp/components/InputTextoPadrao.kt
+++ app/src/main/java/br/com/ecocycle/ecocycleapp/components/InputTextoPadrao.kt
@@ -2,14 +2,12 @@ package br.com.ecocycle.ecocycleapp.components
 
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -17,12 +15,13 @@ import br.com.ecocycle.ecocycleapp.R
 
 @Composable
 fun InputTextoPadrao(
+    modifier: Modifier = Modifier,
     label: String,
     placeholder: String,
     value: String,
     onValueChange: (String) -> Unit,
-    trailingIcon: Int,
-    trailingIconDescription: String
+    trailingIcon: @Composable (() -> Unit)? = null,
+    readOnly: Boolean = false
 ) {
     OutlinedTextField(
         label = { Text(text = label) },
@@ -32,7 +31,9 @@ fun InputTextoPadrao(
         singleLine = true,
         textStyle = TextStyle(fontSize = 16.sp),
         shape = RoundedCornerShape(10.dp),
-        modifier = Modifier.width(300.dp),
+        modifier = Modifier
+            .width(300.dp)
+            .then(modifier),
         colors = OutlinedTextFieldDefaults.colors(
             focusedBorderColor = colorResource(id = R.color.primary),
             focusedTrailingIconColor = colorResource(id = R.color.primary),
@@ -44,11 +45,7 @@ fun InputTextoPadrao(
             unfocusedLabelColor = colorResource(id = R.color.gray),
             focusedPlaceholderColor = colorResource(id = R.color.gray)
         ),
-        trailingIcon = {
-            Icon(
-                painter = painterResource(id = trailingIcon),
-                contentDescription = trailingIconDescription
-            )
-        },
+        trailingIcon = trailingIcon,
+        readOnly = readOnly
     )
 }
