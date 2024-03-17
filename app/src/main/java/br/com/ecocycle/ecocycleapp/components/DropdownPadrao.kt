d78a115ab6244940238ac1c47f8b7420fd2c32a5
adrian-siv <adrian.ssiv@gmail.com>
2024-03-17 19:41:05 -0300

    fix(DropdownPadrao): delete unnecessary padding declaration

--- app/src/main/java/br/com/ecocycle/ecocycleapp/components/DropdownPadrao.kt
+++ app/src/main/java/br/com/ecocycle/ecocycleapp/components/DropdownPadrao.kt
@@ -3,7 +3,6 @@ package br.com.ecocycle.ecocycleapp.components
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.DropdownMenu
