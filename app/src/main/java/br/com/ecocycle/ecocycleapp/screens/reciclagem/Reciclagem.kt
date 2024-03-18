feac41ce95dbe00f9fe8c131b417b8b87df65b96
adrian-siv <adrian.ssiv@gmail.com>
2024-03-18 19:21:29 -0300

    fix(Reciclagem): make screen scrollable

--- app/src/main/java/br/com/ecocycle/ecocycleapp/screens/reciclagem/Reciclagem.kt
+++ app/src/main/java/br/com/ecocycle/ecocycleapp/screens/reciclagem/Reciclagem.kt
@@ -9,7 +9,9 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.OutlinedIconButton
@@ -69,7 +71,8 @@ fun ReciclagemScreen() {
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier
             .fillMaxHeight()
-            .padding(bottom = 30.dp),
+            .padding(bottom = 30.dp)
+            .verticalScroll(rememberScrollState()),
         verticalArrangement = Arrangement.SpaceBetween
     ) {
         Column(
