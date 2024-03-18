8ab252832c68375367ab8403dfd1f5d28f54760b
Laura Trujillo Rosal <lauratrugillo@gmail.com>
2024-03-18 07:50:25 -0300

    tk10

--- app/src/main/java/br/com/ecocycle/ecocycleapp/components/BotaoPadrao.kt
+++ app/src/main/java/br/com/ecocycle/ecocycleapp/components/BotaoPadrao.kt
@@ -35,9 +35,10 @@ fun BotaoPadrao(
                 id = contentColor!!
             )
         ),
-        elevation = ButtonDefaults.buttonElevation(defaultElevation = 20.dp),
+        elevation = ButtonDefaults.buttonElevation(defaultElevation = 8.dp),
         border = BorderStroke(1.dp, colorResource(id = R.color.transparent))
     ) {
         Text(text = text, fontSize = contentFontSize.sp)
     }
 }
+
--- app/src/main/java/br/com/ecocycle/ecocycleapp/screens/reciclagem/Reciclagem.kt
+++ app/src/main/java/br/com/ecocycle/ecocycleapp/screens/reciclagem/Reciclagem.kt
@@ -1,2 +1,20 @@
 package br.com.ecocycle.ecocycleapp.screens.reciclagem
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+
+@Composable
+fun ReciclagemScreen(){
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White)
+    ) {
+        Text(text = "ReciclagemScreen")
+    }
+}
