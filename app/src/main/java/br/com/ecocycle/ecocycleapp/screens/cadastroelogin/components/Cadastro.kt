e60d68cb875b1e2bbcffee2fe28d452e9af19e5f
NicoleTuccio <nicole.tuccio.ribeiro@gmail.com>
2024-03-19 19:50:20 -0300

    TK22 update front

--- app/src/main/java/br/com/ecocycle/ecocycleapp/screens/cadastroelogin/components/Cadastro.kt
+++ app/src/main/java/br/com/ecocycle/ecocycleapp/screens/cadastroelogin/components/Cadastro.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
@@ -21,16 +22,20 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import br.com.ecocycle.ecocycleapp.LogoEcoCycle
 import br.com.ecocycle.ecocycleapp.R
 import br.com.ecocycle.ecocycleapp.components.BotaoPadrao
 import br.com.ecocycle.ecocycleapp.components.InputTextoPadrao
 
 @Composable
 fun CadastroScreen(navController: NavController) {
+
     Box(
         modifier = Modifier
             .fillMaxSize()
@@ -48,95 +53,104 @@ fun CadastroScreen(navController: NavController) {
                     .fillMaxWidth()
                     .height(140.dp)
             ) {
-                Spacer(modifier = Modifier.height(100.dp))
-//                Image(
-//                    painter = painterResource(id = R.drawable.ecocyclelogo),
-//                    contentDescription = "logo"
-//                )
-//                Spacer(modifier = Modifier.height(16.dp))
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier.padding(16.dp)
+                ) {
+                    LogoEcoCycle()
+                    Spacer(modifier = Modifier.width(16.dp))
+                    Text(
+                        text = "EcoCycle",
+                        fontSize = 32.sp,
+                        fontFamily = FontFamily(Font(R.font.inika_bold))
+                    )
+                }
                 Text(
                     text = "Crie sua conta e recicle agora",
                     fontSize = 16.sp
                 )
             }
-            Column(
-                horizontalAlignment = Alignment.CenterHorizontally,
+            Text(
+                text = "Crie sua conta e recicle agora",
+                fontSize = 16.sp
+            )
+        }
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 45.dp)
+        ) {
+            Card(
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 45.dp)
+                    //.height(300.dp)
+                    .fillMaxWidth(),
+                colors = CardDefaults.cardColors(Color.White)
             ) {
-                Card(
-                    modifier = Modifier
-                        //.height(300.dp)
-                        .fillMaxWidth(),
-                    colors = CardDefaults.cardColors(Color.White)
-                ) {
-                    Spacer(modifier = Modifier.height(16.dp))
-                    InputTextoPadrao(
-                        label = "Nome",
-                        placeholder = "Digite seu nome completo",
-                        value = "",
-                        onValueChange = {},
-                        trailingIcon = {
-                            Icon(
-                                painter = painterResource(id = R.drawable.account_box_24),
-                                contentDescription = "Ícone de perfil"
-                            )
-                        }
-                    )
-                    Spacer(modifier = Modifier.height(14.dp))
-                    InputTextoPadrao(
-                        label = "Email",
-                        placeholder = "Digite seu email",
-                        value = "",
-                        onValueChange = {},
-                        trailingIcon = {
-                            Icon(
-                                painter = painterResource(id = R.drawable.mail_icon),
-                                contentDescription = "Ícone de email"
-                            )
-                        }
-                    )
-                    Spacer(modifier = Modifier.height(14.dp))
-                    InputTextoPadrao(
-                        label = "Senha",
-                        placeholder = "Digite sua senha",
-                        value = "",
-                        onValueChange = {},
-                        trailingIcon = {
-                            Icon(
-                                painter = painterResource(id = R.drawable.eye_icon),
-                                contentDescription = "Ícone de olho"
-                            )
-                        }
-                    )
-                    Spacer(modifier = Modifier.height(14.dp))
-                    InputTextoPadrao(
-                        label = "Confirmar senha",
-                        placeholder = "Digite novamente sua senha",
-                        value = "",
-                        onValueChange = {},
-                        trailingIcon = {
-                            Icon(
-                                painter = painterResource(id = R.drawable.eye_icon),
-                                contentDescription = "Ícone de olho"
-                            )
-                        }
-                    )
-                    Spacer(modifier = Modifier.height(24.dp))
+                Spacer(modifier = Modifier.height(16.dp))
+                InputTextoPadrao(
+                    label = "Nome",
+                    placeholder = "Digite seu nome completo",
+                    value = "",
+                    onValueChange = {},
+                    trailingIcon = {
+                        Icon(
+                            painter = painterResource(id = R.drawable.account_box_24),
+                            contentDescription = "Ícone de perfil"
+                        )
+                    }
+                )
+                Spacer(modifier = Modifier.height(14.dp))
+                InputTextoPadrao(
+                    label = "Email",
+                    placeholder = "Digite seu email",
+                    value = "",
+                    onValueChange = {},
+                    trailingIcon = {
+                        Icon(
+                            painter = painterResource(id = R.drawable.mail_icon),
+                            contentDescription = "Ícone de email"
+                        )
+                    }
+                )
+                Spacer(modifier = Modifier.height(14.dp))
+                InputTextoPadrao(
+                    label = "Senha",
+                    placeholder = "Digite sua senha",
+                    value = "",
+                    onValueChange = {},
+                    trailingIcon = {
+                        Icon(
+                            painter = painterResource(id = R.drawable.eye_icon),
+                            contentDescription = "Ícone de olho"
+                        )
+                    }
+                )
+                Spacer(modifier = Modifier.height(14.dp))
+                InputTextoPadrao(
+                    label = "Confirmar senha",
+                    placeholder = "Digite novamente sua senha",
+                    value = "",
+                    onValueChange = {},
+                    trailingIcon = {
+                        Icon(
+                            painter = painterResource(id = R.drawable.eye_icon),
+                            contentDescription = "Ícone de olho"
+                        )
+                    }
+                )
+                Spacer(modifier = Modifier.height(24.dp))
 
-                    BotaoPadrao(
-                        onClick = {
-                            navController.navigate("login")
-                        },
-                        text = "Cadastrar",
-                        contentFontSize = 16
-                    )
-                }
+                BotaoPadrao(
+                    onClick = {
+                        navController.navigate("login")
+                    },
+                    text = "Cadastrar",
+                    contentFontSize = 16
+                )
             }
         }
 
-        //Footer
         Column(
             modifier = Modifier
                 .align(Alignment.BottomCenter)
